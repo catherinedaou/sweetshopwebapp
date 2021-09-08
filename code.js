2eb8df590d9eda6cac8dff4f2bdca2e051c737be
Catherine Daou <catherine.daou@gmail.com>
2021-09-08 09:26:19 +0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,16 @@
+onEvent("image1", "click", function( ) {
+  setProperty("text_area1", "text", "mini cookies");
+  playSound("assets/cookies.mp3", false);
+});
+onEvent("image2", "click", function( ) {
+  setProperty("text_area1", "text", "donuts");
+  playSound("assets/donuts.mp3", false);
+});
+onEvent("image3", "click", function( ) {
+  setProperty("text_area1", "text", "chocolate");
+  playSound("assets/chocolate.mp3", false);
+});
+onEvent("image4", "click", function( ) {
+  setProperty("text_area1", "text", "cupcakes");
+  playSound("assets/cupcakes.mp3", false);
+});
